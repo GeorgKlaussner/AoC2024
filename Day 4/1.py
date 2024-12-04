b80cb43c9623794d87309e929be242bf15e26b68
Klaußner Georg <KlaussnerGeorg@JohnDeere.com>
2024-12-04 13:39:53 +0100

    added day 4 start

--- Day 4/1.py
+++ Day 4/1.py
@@ -1,5 +1,111 @@
 import numpy as np
+shape = None
+matrix = np.array([])
+sum = 0
 
-matrix = np.genfromtxt("input.txt", delimiter=1, dtype=np.character)
+def west(coord):
+    x, y = coord
+    global sum
+    try:
+        if matrix[x-1][y] == b'M' and matrix[x-2][y] == b'A' and matrix[x-3][y] == b'S':
+            sum += 1
+            print(f"w {coord}")
+    except Exception as e:
+        pass
 
-print(sum)
+
+def east(coord):
+    x, y = coord
+    global sum
+    try:
+        if matrix[x+1][y] == b'M' and matrix[x+2][y] == b'A' and matrix[x+3][y] == b'S':
+            sum += 1
+            print(f"e {coord}")
+    except Exception as e:
+        pass
+
+def south(coord):
+    x, y = coord
+    global sum
+    try:
+        if matrix[x][y+1] == b'M' and matrix[x][y+2] == b'A' and matrix[x][y+3] == b'S':
+            sum += 1
+            print(f"s {coord}")
+    except Exception as e:
+        pass
+
+def north(coord):
+    x, y = coord
+    global sum
+    try:
+        if matrix[x][y-1] == b'M' and matrix[x][y-2] == b'A' and matrix[x][y-3] == b'S':
+            sum += 1
+            print(f"n {coord}")
+    except Exception as e:
+        pass
+
+def northWest(coord):
+    x, y = coord
+    global sum
+    try:
+        if matrix[x-1][y-1] == b'M' and matrix[x-2][y-2] == b'A' and matrix[x-3][y-3] == b'S':
+            sum += 1
+            print(f"nw {coord}")
+    except Exception as e:
+        pass
+
+def northEast(coord):
+    x, y = coord
+    global sum
+    try:
+        if matrix[x+1][y-1] == b'M' and matrix[x+2][y-2] == b'A' and matrix[x+3][y-3] == b'S':
+            sum += 1
+            print(f"ne {coord}")
+    except Exception as e:
+        pass
+
+def southEast(coord):
+    x, y = coord
+    global sum
+    try:
+        if matrix[x+1][y+1] == b'M' and matrix[x+2][y+2] == b'A' and matrix[x+3][y+3] == b'S':
+            sum += 1
+            print(f"se {coord}")
+    except Exception as e:
+        pass
+
+def southWest(coord):
+    x, y = coord
+    global sum
+    try:
+        if matrix[x-1][y+1] == b'M' and matrix[x-2][y+2] == b'A' and matrix[x-3][y+3] == b'S':
+            print(f"sw {coord}")
+            sum += 1
+    except Exception as e:
+        pass
+
+
+def main():
+    global shape
+    global matrix
+    matrix = np.genfromtxt("input.txt", delimiter=1, dtype=np.character)
+    shape = matrix.shape
+    a = np.where(matrix == b'X')
+    listA = tuple(zip(*a))
+    for coord in listA:
+        east(coord)
+        west(coord)
+
+        south(coord)
+        north(coord)
+
+        southEast(coord)
+        southWest(coord)
+
+        northEast(coord)
+        northWest(coord)
+    print(sum)
+
+if __name__ == "__main__":
+    main()
+    print(shape)
